5b942b2a4caa630e116cc874efe790dad5fed35f
o0th <o0th@pm.me>
2026-03-09 17:49:04 +0100

    feat: skills

--- app/zr.zig
+++ app/zr.zig
@@ -1,8 +1,16 @@
 const std = @import("std");
 
+pub const Skill = struct {
+    show: bool = true,
+    name: ?[]const u8 = null,
+    exp: ?[]const u8 = null,
+    sub: ?[]const u8 = null,
+};
+
 pub const Resume = struct {
-    name: []const u8,
-    title: []const u8,
+    name: ?[]const u8 = null,
+    title: ?[]const u8 = null,
+    skills: ?[]Skill = null,
 };
 
 const json_bytes = @embedFile("resume.json");
